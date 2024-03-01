7b12c901ce0edb483de5f5222996d68945667163
martinhao56 <martinhaoleonardo252@gmail.com>
2024-02-29 22:36:12 -0300

    lista de municipio

---
+++ json/localidades/script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", async function(){
+    const listaMunicipios = document.getElementById("municipios-list");
+ 
+    const resposta = await fetch('https://servicodados.ibge.gov.br/api/v1/localidades/municipios?orderBy=nome');
+ 
+    const dados = await resposta.json();
+ 
+    dados.forEach(function(municipio){
+        const li = document.createElement('li');
+ 
+        li.textContent = municipio.nome;
+ 
+        listaMunicipios.appendChild(li)
+    });
+}
+ 
+)
